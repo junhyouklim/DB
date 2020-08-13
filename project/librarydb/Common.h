bd912c106e79dfabc2f8aee5058f40867890e3f6
JunHyouk-Lim <40488563+junhyouklim@users.noreply.github.com>
2020-08-13 14:58:56 +0900

    Add files via upload

---
+++ project/librarydb/Common.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <mysql.h>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <Windows.h>
+#define MYSQLIP "10.10.20.229"
+
+using namespace std;
